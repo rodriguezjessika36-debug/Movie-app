5bd44e87d9f844af2385d3641f3c989516aad171
jessica <rodriguezjessika36@gmail.com>
2026-06-16 22:52:12 +0200

    movie list modification

--- src/app/movie-list/movie-list.ts
+++ src/app/movie-list/movie-list.ts
@@ -1,9 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { MoviesService } from '../movies-service';
+import { Movie } from '../movie';
 
 @Component({
   selector: 'app-movie-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './movie-list.html',
   styleUrl: './movie-list.css',
 })
-export class MovieList {}
+export class MovieList {
+  movies: Movie[];
+
+  constructor(private moviesService: MoviesService) {
+    this.movies = this.moviesService.getMovies();
+  }
+}
