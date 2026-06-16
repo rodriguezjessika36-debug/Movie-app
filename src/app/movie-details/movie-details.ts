017f6dae9b569f45763bea424c1d6113fe35f084
jessica <rodriguezjessika36@gmail.com>
2026-06-16 23:06:07 +0200

    movie list

--- src/app/movie-details/movie-details.ts
+++ src/app/movie-details/movie-details.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MoviesService } from '../movies-service';
+import { Movie } from '../movie';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,4 +9,19 @@ import { Component } from '@angular/core';
   templateUrl: './movie-details.html',
   styleUrl: './movie-details.css',
 })
-export class MovieDetails {}
+export class MovieDetails {
+  movie: Movie | undefined;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private moviesService: MoviesService
+  ) {
+    const movieId = this.route.snapshot.paramMap.get('movieId');
+    this.movie = this.moviesService.getMovies().find((m) => m.id === movieId);
+  }
+
+  volverAlListado(): void {
+    this.router.navigate(['/movies']);
+  }
+}
