c0d0b525aa1fd2b6ff42cab27abe6c3d7bfac873
jessica <rodriguezjessika36@gmail.com>
2026-06-16 23:38:16 +0200

    layout refactor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,13 +1,20 @@
 import { Routes } from '@angular/router';
+import { Layout } from './layout/layout';
 import { Home } from './home/home';
 import { MovieList } from './movie-list/movie-list';
 import { MovieDetails } from './movie-details/movie-details';
 import { NotFound } from './not-found/not-found';
 
 export const routes: Routes = [
-  { path: '', component: Home },
-  { path: 'home', redirectTo: '', pathMatch: 'full' },
-  { path: 'movies', component: MovieList },
-  { path: 'movies/:movieId', component: MovieDetails },
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', component: Home },
+      { path: 'home', redirectTo: '', pathMatch: 'full' },
+      { path: 'movies', component: MovieList },
+      { path: 'movies/:movieId', component: MovieDetails },
+    ],
+  },
   { path: '**', component: NotFound },
 ];
