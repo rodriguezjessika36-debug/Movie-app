8b91d818b6a51b3054cef1c744ef154f586944d7
jessica <rodriguezjessika36@gmail.com>
2026-06-17 00:00:49 +0200

    ng build fix

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,16 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'movies/:movieId',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() 
+    {
+      const movieIds = ['1', '2', '3'];
+      return movieIds.map((id) => ({ movieId: id }));
+    },
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
